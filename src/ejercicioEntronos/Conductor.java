e1048d61c55d51502d8ec12dc03d63c29dea69c9
Dani-28305 <158163303+Dani-28305@users.noreply.github.com>
2024-04-02 10:33:07 +0200

    Javadoc Hecho

--- src/ejercicioEntronos/Conductor.java
+++ src/ejercicioEntronos/Conductor.java
@@ -1,5 +1,8 @@
 package ejercicioEntronos;
-
+/**
+ * @author Daniel
+ * @version 1.0
+ */
 public class Conductor {
 	private String nombre;
 	 private int edad;
@@ -7,5 +10,17 @@ public class Conductor {
 		super();
 		this.nombre = nombre;
 		this.edad = edad;
+	}
+	public void aceptarViaje() {
+		/**
+		 * @param nombre Es el nombre del conductor
+		 * @param edad Es la edad del conductor
+		 * @return El destino del viaje y el cliente
+		 */
+	}
+	public void negarViaje() {
+		/**
+		 * @deprecated 
+		 */
 	}
 }
--- src/ejercicioEntronos/Viajero.java
+++ src/ejercicioEntronos/Viajero.java
@@ -1,12 +1,35 @@
 package ejercicioEntronos;
-
+/**
+ * @author Daniel
+ * @version 1.0
+ */
 public class Viajero {
+	
 	private String nombre;
 	private int edad;
 	public Viajero(String nombre, int edad) {
 		super();
 		this.nombre = nombre;
 		this.edad = edad;
-	System.out.println(1);
+		
 	}
+	public void comprarViaje() {
+		/**
+		 * @param nombre Valor que guarda el nombre del viajero 
+		 * @param edad Valor que guarada la edad del viajero
+		 * @since 1.0
+		 * @return Devuelve el viaje que se ha comprado
+		 */
+	}
+	public void darPropina(int cantidad, String nombre) {
+		/**
+		 * @param cantidad Cuanta propina le das al conductor
+		 * @para nombre El nombre del conductor beneficiario
+		 * @see Conductor
+		 * @since 1.0
+		 * @return La propina dada al conductor
+		 */
+		
+	}
+	
 }
