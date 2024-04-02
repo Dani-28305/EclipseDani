e27a171381c7ce6234970424953223daa42d6461
Dani-28305 <158163303+Dani-28305@users.noreply.github.com>
2024-04-02 10:10:29 +0200

    SEGUNDO COMMIT

---
+++ src/ejercicioEntronos/Conductor.java
@@ -0,0 +1,11 @@
+package ejercicioEntronos;
+
+public class Conductor {
+	private String nombre;
+	 private int edad;
+	public Conductor(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+}
---
+++ src/ejercicioEntronos/Viajero.java
@@ -0,0 +1,11 @@
+package ejercicioEntronos;
+
+public class Viajero {
+	private String nombre;
+	private int edad;
+	public Viajero(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+}
