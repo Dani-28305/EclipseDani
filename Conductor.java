4ba039aa3f66027d2d781a8cc58b4be7db441842
Dani-28305 <158163303+Dani-28305@users.noreply.github.com>
2024-04-02 10:37:05 +0200

    Add files via upload

---
+++ Conductor.java
@@ -0,0 +1,26 @@
+package ejercicioEntronos;
+/**
+ * @author Daniel
+ * @version 1.0
+ */
+public class Conductor {
+	private String nombre;
+	 private int edad;
+	public Conductor(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	public void aceptarViaje() {
+		/**
+		 * @param nombre Es el nombre del conductor
+		 * @param edad Es la edad del conductor
+		 * @return El destino del viaje y el cliente
+		 */
+	}
+	public void negarViaje() {
+		/**
+		 * @deprecated 
+		 */
+	}
+}
